0c92c5c0fb8a67eeb033096ef8a26fdc41cfbe0b
yogithaml46 <102967244+yogithaml46@users.noreply.github.com>
2023-02-07 18:52:34 +0530

    Create dijkstra.cpp

---
+++ dijkstra.cpp
@@ -0,0 +1,65 @@
+#include<iostream>
+#include<stdio.h>
+using namespace std;
+#define INFINITY 9999
+#define max 5
+void dijkstra(int G[max][max],int n,int startnode) {
+   int cost[max][max],distance[max],pred[max];
+   int visited[max],count,mindistance,nextnode,i,j;
+   for(i=0;i<n;i++)
+      for(j=0;j<n;j++)
+   if(G[i][j]==0)
+      cost[i][j]=INFINITY;
+   else
+      cost[i][j]=G[i][j];
+   for(i=0;i<n;i++) {
+      distance[i]=cost[startnode][i];
+      pred[i]=startnode;
+      visited[i]=0;
+   }
+   distance[startnode]=0;
+   visited[startnode]=1;
+   count=1;
+   while(count<n-1) {
+      mindistance=INFINITY;
+      for(i=0;i<n;i++)
+         if(distance[i]<mindistance&&!visited[i]) {
+         mindistance=distance[i];
+         nextnode=i;
+      }
+      visited[nextnode]=1;
+      for(i=0;i<n;i++)
+         if(!visited[i])
+      if(mindistance+cost[nextnode][i]<distance[i]) {
+         distance[i]=mindistance+cost[nextnode][i];
+         pred[i]=nextnode;
+      }
+      count++;
+   }
+   for(i=0;i<n;i++)
+   if(i!=startnode) {
+      cout<<"\nDistance of node "<<(i+1)<<" = "<<distance[i];
+      cout<<"\nPath="<<i;
+      j=i;
+      do {
+         j=pred[j];
+         cout<<"<-"<<j;
+      }while(j!=startnode);
+   }
+}
+
+
+int main() {
+     int graph[max][max] ;
+    cout<<"Enter the graph "<<endl;
+    for(int i = 0; i<max; i++)
+    {
+        for(int j = 0; j<max; j++)
+            cin>>graph[i][j];
+    }
+   int n=5;
+   int u=0;
+    cout<<"\nThe shortest path from source:\n";
+   dijkstra(graph,n,u);
+   return 0;
+}
